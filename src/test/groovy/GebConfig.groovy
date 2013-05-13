cd2626012842c8cdd3b2271394a868d4731d91ac
corewalk <mihai@corewalk.com>
2013-05-13 20:17:18 +0300

    Update GebConfig.groovy

--- src/test/groovy/GebConfig.groovy
+++ src/test/groovy/GebConfig.groovy
@@ -2,10 +2,16 @@ import org.openqa.selenium.firefox.FirefoxDriver
 import org.openqa.selenium.remote.RemoteWebDriver
 import java.util.logging.Level
 
+** remote** remote** remote** remote** remote
+** remote** remote** remote** remote** remote
+
 driver = {	
 	RemoteWebDriver driver = new FirefoxDriver()
 	driver.setLogLevel(Level.WARNING)
 	driver.config.abc
 	add test here
+	** remote
 	driver
 }
+
+** remote** remote** remote
